8dcf8cf685b884788aa26e0191b73bcc16bd6bb9
Samuel Cochran <sj26@sj26.com>
2014-06-18 15:03:45 +1000

    Allow adding custom boot files
    Makes including jasmine-jquery-rails easier.

--- app/helpers/jasmine_rails/spec_runner_helper.rb
+++ app/helpers/jasmine_rails/spec_runner_helper.rb
@@ -21,22 +21,10 @@ module JasmineRails
     # * jasmine-specs.js built by asset pipeline which merges application specific libraries and specs
     def jasmine_js_files
       files = Jasmine::Core.js_files
-      files << jasmine_boot_file
+      files += JasmineRails.boot_files
       files += JasmineRails.reporter_files params[:reporters]
       files << 'jasmine-specs.js'
       files
-    end
-
-    def jasmine_boot_file
-      if jasmine2?
-        Jasmine::Core.boot_files.first
-      else
-        'jasmine-boot.js'
-      end
-    end
-
-    def jasmine2?
-      Jasmine::Core.respond_to?(:boot_files)
     end
   end
 end
